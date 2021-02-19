29a90ac8eb9f7c15d3102fba6a98c4ebe218193b
Arthur Walsh <walsh.arthur@outlook.com>
2021-02-19 17:40:47 +0100

    fix

--- redux/reducer/UsersReducer.ts
+++ redux/reducer/UsersReducer.ts
@@ -23,8 +23,8 @@ const usersReducer = (state = initialState, action: TUsersActionsTypes) => {
       return { ...state, users: act.users, count: act.users.length }
     case constants.USERS.USERS_REMOVE:
       act = action as IUsersDelete
-      const id = act.user.id
-      const index = state.users.findIndex(elem => elem.id === id)
+      const id = act.user._id
+      const index = state.users.findIndex(elem => elem._id === id)
       const newUsers = [
         ...state.users.slice(0, index),
         ...state.users.slice(index + 1)
